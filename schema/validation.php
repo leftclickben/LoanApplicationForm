45c8bc091da66c96972c49354b60583835f3d176
leftclickben <ben@leftclick.com.au>
2013-09-23 19:26:15 +0800

    Tweaks to validation and form layouts

--- schema/validation.php
+++ schema/validation.php
@@ -5,26 +5,67 @@ return call_user_func(function () {
 	 * Messages, collected here for ease of modification and potential reuse
 	 */
 	$messages = array(
-		'loan_amount' => array(
-			'out_of_range' => 'Loan amount must be between $100 and $2000'
+		'application' => array(
+			'loan_amount' => array(
+				'out_of_range' => 'Loan amount must be between $100 and $2000'
+			),
+			'loan_term_weeks' => array(
+				'invalid' => 'Only Australian residents can apply for a 12 month loan'
+			)
 		),
-		'loan_term_weeks' => array(
-			'invalid' => 'Only Australian residents can apply for a 12 month loan'
+		'personal' => array(
+			'first_name' => array(
+				'required' => 'Please supply your first name'
+			),
+			'last_name' => array(
+				'required' => 'Please supply your last name'
+			),
+			'date_of_birth' => array(
+				'required' => 'Please supply your birth date'
+			),
+			'email' => array(
+				'match' => 'Please supply your email address in the form "user@domain.com"'
+			),
+			'phone_primary' => array(
+				'match' => 'Please supply your 10-digit Australian phone number including area code'
+			),
+			'phone_secondary' => array(
+				'match' => 'Please supply your 10-digit Australian phone number including area code'
+			)
 		),
-		'first_name' => array(
-			'required' => 'Please supply your first name'
+		'address' => array(
+			'address_line1' => array(
+				'required' => 'Please supply the street address'
+			),
+			'suburb' => array(
+				'required' => 'Please supply the suburb'
+			),
+			'postcode' => array(
+				'required' => 'Please supply the postcode'
+			),
+			'state' => array(
+				'required' => 'Please supply your state'
+			),
+			'duration_months' => array(
+				'required' => 'Please indicate how long you have lived at this address'
+			),
 		),
-		'last_name' => array(
-			'required' => 'Please supply your last name'
-		),
-		'email' => array(
-			'match' => 'Please supply your email address in the form "user@domain.com"'
-		),
-		'phone_primary' => array(
-			'match' => 'Please supply your 10-digit Australian phone number including area code'
-		),
-		'phone_secondary' => array(
-			'match' => 'Please supply your 10-digit Australian phone number including area code'
+		'income' => array(
+			'type' => array(
+				'required' => 'Please select the type of income you receive'
+			),
+			'amount' => array(
+				'required' => 'Please supply your income amount in whole dollars'
+			),
+			'period' => array(
+				'required' => 'Please indicate how often you are paid'
+			),
+			'duration_months' => array(
+				'required' => 'Please indicate how long you have received this income'
+			),
+			'description' => array(
+				'required' => 'Please describe the source of your income'
+			),
 		)
 	);
 
@@ -128,8 +169,9 @@ return call_user_func(function () {
 			return $pass($entity, $field);
 		},
 		'application' => array(
+			// TODO Validate number format
 			'loan_amount' => function($application, $value) use ($messages, $range) {
-				return $range('application', 'loan_amount', $value, 100, 2000, $messages['loan_amount']['out_of_range']);
+				return $range('application', 'loan_amount', $value, 100, 2000, $messages['application']['loan_amount']['out_of_range']);
 			},
 			'australian_resident' => function($application, $value) use ($messages) {
 				if ($value || $application['loan_term_weeks'] !== 6) {
@@ -151,7 +193,7 @@ return call_user_func(function () {
 						array(
 							'entity' => 'application',
 							'field' => 'loan_term_weeks',
-							'message' => $messages['loan_term_weeks']['invalid']
+							'message' => $messages['application']['loan_term_weeks']['invalid']
 						)
 					);
 				}
@@ -176,7 +218,7 @@ return call_user_func(function () {
 						array(
 							'entity' => 'application',
 							'field' => 'loan_term_weeks',
-							'message' => $messages['loan_term_weeks']['invalid']
+							'message' => $messages['application']['loan_term_weeks']['invalid']
 						)
 					);
 				}
@@ -184,29 +226,66 @@ return call_user_func(function () {
 		),
 		'personal' => array(
 			'first_name' => function($application, $value) use ($messages, $required) {
-				return $required('personal', 'first_name', $value, $messages['first_name']['required']);
+				return $required('personal', 'first_name', $value, $messages['personal']['first_name']['required']);
 			},
 			'middle_name' => function($application, $value) use ($pass) {
 				return $pass('personal', 'middle_name');
 			},
 			'last_name' => function($application, $value) use ($messages, $required) {
-				return $required('personal', 'last_name', $value, $messages['last_name']['required']);
+				return $required('personal', 'last_name', $value, $messages['personal']['last_name']['required']);
+			},
+			// TODO Validate date format
+			'date_of_birth' => function($application, $value) use ($messages, $required) {
+				return $required('personal', 'date_of_birth', $value, $messages['personal']['date_of_birth']['required']);
 			},
 			'email' => function($application, $value) use ($messages, $regexPatterns, $match) {
-				return $match('personal', 'email', $value, $regexPatterns['email'], true, $messages['email']['match']);
+				return $match('personal', 'email', $value, $regexPatterns['email'], true, $messages['personal']['email']['match']);
 			},
 			'phone_primary' => function($application, $value) use ($messages, $regexPatterns, $match) {
-				return $match('personal', 'phone_primary', $value, $regexPatterns['phone'], true, $messages['phone_primary']['match']);
+				return $match('personal', 'phone_primary', $value, $regexPatterns['phone'], true, $messages['personal']['phone_primary']['match']);
 			},
 			'phone_secondary' => function($application, $value) use ($messages, $regexPatterns, $match) {
-				return $match('personal', 'phone_secondary', $value, $regexPatterns['phone'], false, $messages['phone_secondary']['match']);
+				return $match('personal', 'phone_secondary', $value, $regexPatterns['phone'], false, $messages['personal']['phone_secondary']['match']);
 			}
 		),
 		'address' => array(
-
+			'address_line1' => function($application, $value) use ($messages, $required) {
+				return $required('address', 'address_line1', $value, $messages['address']['address_line1']['required']);
+			},
+			'address_line2' => function($application, $value) use ($messages, $pass) {
+				return $pass('address', 'address_line2');
+			},
+			'suburb' => function($application, $value) use ($messages, $required) {
+				return $required('address', 'suburb', $value, $messages['address']['suburb']['required']);
+			},
+			'postcode' => function($application, $value) use ($messages, $required) {
+				return $required('address', 'postcode', $value, $messages['address']['postcode']['required']);
+			},
+			'state' => function($application, $value) use ($messages, $required) {
+				return $required('address', 'state', $value, $messages['address']['state']['required']);
+			},
+			// TODO Validate number format
+			'duration_months' => function($application, $value) use ($messages, $required) {
+				return $required('address', 'duration_months', $value, $messages['address']['duration_months']['required']);
+			},
 		),
 		'income' => array(
-
+			'type' => function($application, $value) use ($messages, $required) {
+				return $required('income', 'type', $value, $messages['income']['type']['required']);
+			},
+			// TODO Validate number format
+			'amount' => function($application, $value) use ($messages, $required) {
+				return $required('income', 'amount', $value, $messages['income']['amount']['required']);
+			},
+			'period' => function($application, $value) use ($messages, $required) {
+				return $required('income', 'period', $value, $messages['income']['period']['required']);
+			},
+			'duration_months' => function($application, $value) use ($messages, $required) {
+				return $required('income', 'duration_months', $value, $messages['income']['duration_months']['required']);
+			},
+			'description' => function($application, $value) use ($messages, $required) {
+				return $required('income', 'description', $value, $messages['income']['description']['required']);
+			},
 		)
 	);
 });
